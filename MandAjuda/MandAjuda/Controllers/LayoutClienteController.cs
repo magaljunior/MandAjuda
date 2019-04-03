87015feddc65cd7641743703bfd64bfa33d94b7a
Pedro Henrique Damascena Santos <selmasilvasantos2013@live.com>
2019-04-03 19:50:13 -0300

    login

---
+++ MandAjuda/MandAjuda/Controllers/LayoutClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LayoutClienteController : Controller
+    {
+        // GET: LayoutCliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
