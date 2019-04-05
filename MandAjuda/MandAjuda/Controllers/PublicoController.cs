2cddcee1af2396ce894f21398569bfbd4cd7561e
Pedro Henrique Damascena Santos <selmasilvasantos2013@live.com>
2019-04-04 22:01:09 -0300

    .

--- MandAjuda/MandAjuda/Controllers/PublicoController.cs
+++ MandAjuda/MandAjuda/Controllers/PublicoController.cs
@@ -1,67 +1,67 @@
-﻿using MandAjuda.Models;
-using MandAjuda.Repositories;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MandAjuda.Models;
+using MandAjuda.Repositories;
 
-namespace MandAjuda.Controllers
+namespace WebApplication1.Controllers
 {
-	public class PublicoController : Controller
-	{
-		// GET: Publico
-		public ActionResult LogarCliente()
-		{
-			return View();
-		}
+    public class PublicoController : Controller
+    {
+        // GET: Publico
+        public ActionResult LogarCliente()
+        {
+            return View();
+        }
 
-		public ActionResult LogarProfissional()
-		{
-			return View();
-		}
+        public ActionResult LogarProfissional()
+        {
+            return View();
+        }
 
-		[HttpPost]
-		public ActionResult LogarCliente( string email, string senha)
-		{
-			if (Funcoes.AutenticarUsuarioCliente(email, senha) == false)
-			{
-				ViewBag.Error = "Nome de usuário e/ou senha inválida";
-				return View();
-			}
-			return RedirectToAction("Index", "LayoutCliente");
-		}
+        [HttpPost]
+        public ActionResult LogarCliente(string email, string senha)
+        {
+            if (Funcoes.AutenticarUsuarioCliente(email, senha) == false)
+            {
+                ViewBag.Error = "Nome de usuário e/ou senha inválida";
+                return View();
+            }
+            return RedirectToAction("Index", "LayoutCliente");
+        }
 
-		[HttpPost]
-		public ActionResult LogarProfissional( string email, string senha)
-		{
-			if (Funcoes.AutenticarUsuarioProfissional(email, senha) == false)
-			{
-				ViewBag.Error = "Nome de usuário e/ou senha inválida";
-				return View();
-			}
-			return RedirectToAction("Index", "LayoutProfissional");
-		}
+        [HttpPost]
+        public ActionResult LogarProfissional(string email, string senha)
+        {
+            if (Funcoes.AutenticarUsuarioProfissional(email, senha) == false)
+            {
+                ViewBag.Error = "Nome de usuário e/ou senha inválida";
+                return View();
+            }
+            return RedirectToAction("Index", "LayoutProfissional");
+        }
 
-		public ActionResult LogoffCliente()
-		{
-			MandAjuda.Repositories.Funcoes.Deslogar();
-			return RedirectToAction("LogarCliente", "Publico");
-		}
+        public ActionResult LogoffCliente()
+        {
+            MandAjuda.Repositories.Funcoes.Deslogar();
+            return RedirectToAction("LogarCliente", "Publico");
+        }
 
-		public ActionResult LogoffProfissional()
-		{
-			MandAjuda.Repositories.Funcoes.Deslogar();
-			return RedirectToAction("LogarProfissional", "Publico");
-		}
+        public ActionResult LogoffProfissional()
+        {
+            MandAjuda.Repositories.Funcoes.Deslogar();
+            return RedirectToAction("LogarProfissional", "Publico");
+        }
 
-		public ActionResult AcessoNegado()
-		{
-			using (Context c = new Context())
-			{
-				return View();
-			}
-		}
+        public ActionResult AcessoNegado()
+        {
+            using (Context c = new Context())
+            {
+                return View();
+            }
+        }
 
-	}
+    }
 }
