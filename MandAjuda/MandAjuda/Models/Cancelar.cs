a7afe36498ddcd7f91d3779dcae73b178e130a9a
Pedro Henrique Damascena Santos <selmasilvasantos2013@live.com>
2019-04-05 14:43:08 -0300

    Layout

--- MandAjuda/MandAjuda/Models/Cancelar.cs
+++ MandAjuda/MandAjuda/Models/Cancelar.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +9,16 @@ namespace MandAjuda.Models
 {
     public class Cancelar
     {
+        [DisplayName("De:")]
         public string From { get; set; }
+        [Required(ErrorMessage = "Por favor digite o email do profissional")]
+        [DisplayName("Para:")]
         public string To { get; set; }
+        [Required(ErrorMessage = "Por favor informe o motivo do cancelamento do chamado")]
+        [DisplayName("Motivo:")]
         public string Subject { get; set; }
+        [Required(ErrorMessage = "Por favor informe uma breve descrição do motivo do cancelamento do chamado")]
+        [DisplayName("Descrição:")]
         public string Body { get; set; }
     }
 }
