0ed7160d1e81013aa7b6a60acadb6398c77e18bf
PEDRO SANTOS <pedro.santos92@fatec.sp.gov.br>
2019-05-28 08:15:50 -0300

    .

--- MandAjuda/MandAjuda/Migrations/201905222224502_profissional.Designer.cs
+++ MandAjuda/MandAjuda/Migrations/201905222224502_profissional.Designer.cs
@@ -7,13 +7,13 @@ namespace MandAjuda.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class curriculum : IMigrationMetadata
+    public sealed partial class profissional : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(curriculum));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(profissional));
         
         string IMigrationMetadata.Id
         {
-            get { return "201905180005462_curriculum"; }
+            get { return "201905222224502_profissional"; }
         }
         
         string IMigrationMetadata.Source
